1be4374ca7ac353f9de88b641e195d618b66bf10
Ekaterina Novikova <fearnocait@gmail.com>
2025-11-28 13:24:46 +0300

    [fix] конфиги для форматтеров

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules', 'public', 'build']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -15,6 +15,11 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    settings: {
+      react: {
+          version: 'detect',
+      },
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
