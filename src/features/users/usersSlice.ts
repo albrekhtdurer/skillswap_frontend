01523cc189738d47e61bc529a01f797075cac68a
agilina52 <agilina52@yandex.ru>
2025-12-01 23:40:37 +0300

    доработка стилей

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -1,60 +1,61 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { api } from "../../api";
-import type { User } from '../../entities/types';
+import type { User } from "../../entities/types";
 
 type TUsersState = {
-    users: User[];
-    loading: boolean;
-    error: string | undefined;
-    selectedUserId: number | null;
+  users: User[];
+  loading: boolean;
+  error: string | undefined;
+  selectedUserId: number | null;
 };
 
 const initialState: TUsersState = {
-    users: [],
-    loading: false,
-    error: undefined,
-    selectedUserId: null
+  users: [],
+  loading: false,
+  error: undefined,
+  selectedUserId: null,
 };
 
 export const getUsers = createAsyncThunk("users/getUsers", api.getAllUsers);
 
 export const usersSlice = createSlice({
-    name: "users",
-    initialState,
-    reducers: {
-        selectUser: (state, action) => { state.selectedUserId = action.payload }, //для просмотра детальной информации
-        clearSelectedUser: (state) => { state.selectedUserId = null },
+  name: "users",
+  initialState,
+  reducers: {
+    selectUser: (state, action) => {
+      state.selectedUserId = action.payload;
+    }, //для просмотра детальной информации
+    clearSelectedUser: (state) => {
+      state.selectedUserId = null;
     },
-    selectors: {
-        usersSelector: (state) => state.users,
-        usersLoadingSelector: (state) => state.loading,
-        selectedUserSelector: (state) => {
-            return state.users.find(user => user.id === state.selectedUserId);
-        },
+  },
+  selectors: {
+    usersSelector: (state) => state.users,
+    usersLoadingSelector: (state) => state.loading,
+    selectedUserSelector: (state) => {
+      return state.users.find((user) => user.id === state.selectedUserId);
     },
-    extraReducers: (builder) => {
-        builder
-            .addCase(getUsers.pending, (state) => {
-                state.loading = true;
-                state.error = undefined;
-                console.log("Загрузка пользователей...");
-            })
-            .addCase(getUsers.rejected, (state, action) => {
-                state.loading = false;
-                state.error = action.error.message;
-                console.log("Ошибка загрузки пользователей:", action.error.message);
-            })
-            .addCase(getUsers.fulfilled, (state, action) => {
-                state.loading = false;
-                state.users = action.payload;
-                console.log(`Пользователи загружены: ${action.payload.length} записей`);
-            });
-    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getUsers.pending, (state) => {
+        state.loading = true;
+        state.error = undefined;
+        console.log("Загрузка пользователей...");
+      })
+      .addCase(getUsers.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+        console.log("Ошибка загрузки пользователей:", action.error.message);
+      })
+      .addCase(getUsers.fulfilled, (state, action) => {
+        state.loading = false;
+        state.users = action.payload;
+        console.log(`Пользователи загружены: ${action.payload.length} записей`);
+      });
+  },
 });
 
 export const { selectUser, clearSelectedUser } = usersSlice.actions;
-export const {
-    usersSelector,
-    usersLoadingSelector,
-    selectedUserSelector
-} = usersSlice.selectors;
+export const { usersSelector, usersLoadingSelector, selectedUserSelector } =
+  usersSlice.selectors;
