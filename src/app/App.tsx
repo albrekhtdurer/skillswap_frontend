c3948f0eada9e8291e05b4449763c67d3a85c0e0
Rita Tcepeleva <tsepeleva.rita@mail.ru>
2025-12-02 11:24:00 +0300

    fix: clean App component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,5 @@
-import { Footer } from "../widgets/footer/footer";
-
 function App() {
-  return (
-    <div>
-      <Footer allSkillsOnClick={() => console.log("all skills click")} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
