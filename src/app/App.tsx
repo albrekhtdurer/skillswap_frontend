92932de6b3c23accc6ce7578b90827530eca6830
Ekaterina Novikova <fearnocait@gmail.com>
2025-12-02 18:27:48 +0300

    [misc] удалила тестовое

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,41 +1,5 @@
-import { useState } from "react";
-import { RadioGroup } from "../shared/ui/RadioGroup/RadioGroup";
-import type { OptionType } from "../shared/ui/RadioGroup/Option";
-
 function App() {
-  const options = [
-    {
-      title: "Не имеет значения",
-      value: "any",
-    },
-    {
-      title: "Мужской",
-      value: "male",
-    },
-    {
-      title: "Женский",
-      value: "female",
-    },
-  ];
-
-  const [state, setState] = useState(options[0]);
-
-  const handleChange = (option: OptionType) => {
-    setState(option);
-    console.log(option);
-  };
-
-  return (
-    <div>
-      <RadioGroup
-        name="authorSex"
-        onChange={handleChange}
-        options={options}
-        selected={state}
-        title="Пол автора"
-      />
-    </div>
-  );
+  return <div></div>
 }
 
 export default App;
