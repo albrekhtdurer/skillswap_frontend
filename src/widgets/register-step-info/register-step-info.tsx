3e9aee59cc0d223dad18672851b6186b0f114a74
Julya <julya_mka@yandex.ru>
2025-12-03 15:50:43 +0300

    feat: добавление виджета для регистрации

---
+++ src/widgets/register-step-info/register-step-info.tsx
@@ -0,0 +1,23 @@
+import { type FC } from "react";
+import styles from "./register-step-info.module.css";
+
+interface RegisterStepInfoProps {
+  image: string;
+  title: string;
+  subtitle: string;
+}
+
+export const RegisterStepInfo: FC<RegisterStepInfoProps> = ({
+  image,
+  title,
+  subtitle,
+}) => {
+  return (
+    <div className={styles.wrapper}>
+      <img src={image} alt="" className={styles.image} />
+
+      <h2 className={styles.title}>{title}</h2>
+      <p className={styles.subtitle}>{subtitle}</p>
+    </div>
+  );
+};
