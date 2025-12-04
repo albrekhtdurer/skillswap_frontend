3efac8a320daa0c7ce630d396c9b1cbb793402ca
Ekaterina Novikova <fearnocait@gmail.com>
2025-12-04 13:13:23 +0300

    [misc] удалила тест данные

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,5 @@
-import { Sidebar } from "../widgets/sidebar";
-import { useDispatch } from "../features/store";
-import { useEffect } from "react";
-
-import { getCategories } from "../features/categories/categoriesSlice";
-import { getCities } from "../features/cities/citiesSlice";
-
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getCategories());
-    dispatch(getCities());
-  }, []);
-
-  return (
-    <div style={{ height: "100vh" }}>
-      <Sidebar />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
