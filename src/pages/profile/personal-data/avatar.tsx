fb887445dc17f15ed46cc355eaa1ab48ce69a52c
Ekaterina Novikova <fearnocait@gmail.com>
2025-12-18 18:06:46 +0300

    [fix] изначальные стили и форма

--- src/pages/profile/personal-data/avatar.tsx
+++ src/pages/profile/personal-data/avatar.tsx
@@ -1,22 +1,32 @@
 import { AvatarUpload } from "../../../shared/ui/avatar-upload/avatar-upload";
 import EditIcon from "../../../assets/icons/edit-image.svg";
 import styles from "./avatar.module.css";
-import { useAvatar } from "../../../shared/hooks/useAvatar";
 import PlaceholderIcom from "../../../assets/icons/avatar-placeholder.svg";
+import { useSelector } from "../../../features/store";
+import { selectCurrentUser } from "../../../features/user/userSlice";
 
-export function ProfileAvatar() {
-  const { avatarFile, setAvatar } = useAvatar();
+type ProfileAvatarFieldProps = {
+  avatarUrl?: string;
+  onAvatarChange: (file: File | null) => void;
+};
+
+export function ProfileAvatar({
+  avatarUrl,
+  onAvatarChange,
+}: ProfileAvatarFieldProps) {
+  const currentUser = useSelector(selectCurrentUser);
+  const avatarSrc = avatarUrl || currentUser?.avatarUrl || undefined;
 
   return (
     <AvatarUpload
       size={244}
-      imageUrl={avatarFile ? URL.createObjectURL(avatarFile) : undefined}
+      imageUrl={avatarSrc}
       placeholderIcon={PlaceholderIcom}
       buttonIcon={
         <img src={EditIcon} alt="" className={styles.profileAvatarIcon} />
       }
       buttonPlacement="bottom-right"
-      onChange={setAvatar}
+      onChange={onAvatarChange}
       buttonClassName={styles.profileAvatarButton}
     />
   );
